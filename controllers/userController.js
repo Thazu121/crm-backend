23bbdf47166c37cce9a5cc5edbcd6bdabf1cef07
Thasleema <thasleemapk133@gmail.com>
2026-04-29 20:40:55 +0530

    Implement login API with JWT authentication and secure user response

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ const registerUser = async (req, res, next) => {
 
         }
         name = name.trim()
-    email = email.trim().toLowerCase()
+        email = email.trim().toLowerCase()
         password = password.trim()
         if (name.length < 3) {
             return res.status(400).json({ message: "Name must contain at least 3 characters" })
@@ -29,7 +29,7 @@ const registerUser = async (req, res, next) => {
                     "Password must be at least 6 characters with uppercase, lowercase, and number"
             });
         }
-        const existingUser = await userModel.findOne({email})
+        const existingUser = await userModel.findOne({ email })
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" })
 
@@ -52,5 +52,51 @@ const registerUser = async (req, res, next) => {
         next(err)
     }
 }
+const loginUser = async (req, res, next) => {
+    try {
+        let { email, password } = req.body
+        email=email.trim().toLowerCase()
+        password=password.trim()
+        if (!email || !password) {
+            return res.status(400).json({ message: 'All fields are required' })
+        }
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" })
+
+        }
+ if (!process.env.JWT_SECRET) {
+            const err = new Error("Server configuration error")
+            err.statusCode = 500
+            return next(err)
+        }
+        const token=jwt.sign({
+            id:user._id,
+            name:user.name
+        },
+     process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        )
+         const userResponse = user.toObject()
+        delete userResponse.password
+
+        return res.status(200).json({
+            message: "Login success",
+            token,
+            user: userResponse
+        })
+    } catch (error) {
+next(error)
+    }
 
 
+}
+export {
+    registerUser,
+    loginUser
+}
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import userRouter from "./routes/userRoute"
 
 dotenv.config()
 const app=express()
 app.use(express.json())
+app.use("/",userRouter)
 const PORT = process.env.PORT 
 connectDB()
 
