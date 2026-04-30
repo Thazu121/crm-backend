9012ad5395d5e0d7f207efd10418ea1e00541225
Thasleema <thasleemapk133@gmail.com>
2026-04-30 16:04:05 +0530

    Fix auth middleware and finalize JWT route protection

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+const authMiddleware=async (req,res,next) => {
+    try {
+          const authHeader=req.headers.authorization
+    console.log(authHeader);
+    if(!authHeader|| !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({message:"Unauthorized"})
+    }
+    const token=authHeader.split(" ")[1] 
+    if(!process.env.JWT_SECRET){
+        return res.status(500).json({ message: "Server configuration error" })
+    }
+const decoded=jwt.verify(token,process.env.JWT_SECRET)
+req.user=decoded
+next()
+} catch (error) {
+                return res.status(401).json({ message: "Forbidden" });
+
+    }
+  
+
+
+    
+    
+}
+export default authMiddleware
